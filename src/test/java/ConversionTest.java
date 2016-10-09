932baeddfa7bd2eac7287e1dd711906854500b61
Szymon Sieciński <szymon.siecinski@gmail.com>
2016-10-09 14:22:04 +0200

    wrzutka na repo

--- src/test/java/ConversionTest.java
+++ src/test/java/ConversionTest.java
@@ -34,17 +34,37 @@ public class ConversionTest {
     }
 
     @Test
+    public void shouldReturnCurrencyToPlnRateAmountWhenTargetCurrencyIsPln() {
+        Conversion conversion = new Conversion();
+        Currency usd = new Currency("USD", "dolar amerykański", 3.8505);
+        Currency pln = new Currency("PLN", "złoty polski", 1);
+        double amount = 1;
+        conversion.setSourceCurrency(usd);
+        conversion.setTargetCurrency(pln);
+        conversion.setAmount(amount);
+
+        try {
+            double calculated = conversion.perform();
+            double expected = 3.85;
+
+            assertEquals(expected, calculated, 0.01);
+        }
+        catch(NonInitializedException e) {}
+    }
+
+    @Test
     public void shouldReturnNewAmountWhenSourceAndTargetCurrencyAreDifferent() {
         Conversion conversion = new Conversion();
         Currency usd = new Currency("USD", "dolar amerykański", 3.8505);
         Currency czk = new Currency("CZK", "korona czeska", 0.1586);
-        double expected = 2427.80;
+        double amount = 100;
         conversion.setSourceCurrency(usd);
         conversion.setTargetCurrency(czk);
-        conversion.setAmount(expected);
+        conversion.setAmount(amount);
 
         try {
             double calculated = conversion.perform();
+            double expected = 2427.8;
 
             assertEquals(expected, calculated, 0.01);
         }
